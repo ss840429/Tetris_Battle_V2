e2e57c2f8e054aba438055505a48ac6cd091b11b
Wlogsky <ss840429@gmail.com>
2017-02-05 00:08:29 +0800

    Play Music!

---
+++ include/Audio.h
@@ -0,0 +1,10 @@
+#ifndef AUDIO_H_INCLUDED
+#define AUDIO_H_INCLUDED
+
+bool initAudio();
+void playMusic();
+void playSound();
+
+
+
+#endif // AUDIO_H_INCLUDED
---
+++ src/Audio.cpp
@@ -0,0 +1,63 @@
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string>
+
+
+sf::Music music;
+sf::SoundBuffer soundBuffer;
+sf::Sound sound;
+
+bool initAudio()
+{
+    if (!music.openFromFile("music/LOL.wav"))
+        return false;
+    if (!soundBuffer.loadFromFile("music/don.wav"))
+        return false;
+
+    sound.setBuffer(soundBuffer);
+
+    return true;
+}
+
+
+void playm()
+{
+    music.play();
+
+    while (music.getStatus() == sf::Music::Playing)
+    {
+        sf::sleep(sf::seconds(100));    // Reduce CPU time
+
+        /*
+        // Display the playing position
+        std::cout << "\rPlaying... " << music.getPlayingOffset().asSeconds() << " sec        ";
+        std::cout << std::flush;
+        */
+    }
+
+
+}
+void plays()
+{
+    sound.play();
+
+    while (sound.getStatus() == sf::Sound::Playing)
+    {
+        sf::sleep(sf::milliseconds(100));    // Reduce CPU time
+    }
+
+}
+
+sf::Thread threadm(&playm);
+sf::Thread threads(&plays);
+
+
+void playMusic()
+{
+    threadm.launch();
+
+}
+void playSound()
+{
+    threads.launch();
+}
